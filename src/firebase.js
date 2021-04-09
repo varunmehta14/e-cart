6fc4beebefa01e604cbbfcb10a7d89e740b7d143
varunmehta14 <mehtavarunj@gmail.com>
2021-04-09 13:22:14 +0530

    Images added

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import  firebase from 'firebase';
 var firebaseConfig = {
-    apiKey: "*******",
+    apiKey: "AIzaSyAnxB1M1TR2MRvJfJTbsQab_jfn4vPw6Tk",
     authDomain: "e-cart-18590.firebaseapp.com",
     databaseURL: "https://e-cart-18590-default-rtdb.firebaseio.com",
     projectId: "e-cart-18590",
     storageBucket: "e-cart-18590.appspot.com",
-    messagingSenderId: "******",
-    appId: "**********",
-    measurementId: "********"
+    messagingSenderId: "1077259714425",
+    appId: "1:1077259714425:web:4900c5ec69ce2d34f579a8",
+    measurementId: "G-XQT0ZVL11C"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
